8164bc801b5954ce9072be7a4b3b38f2df01abe5
genouf <gabriel.enouf@yahoo.fr>
2023-02-22 23:54:14 +0100

    [ADD] v1 fonctionnel

--- srcs/requirements/mariadb/tools/db_creation.sh
+++ srcs/requirements/mariadb/tools/db_creation.sh
@@ -2,10 +2,15 @@
 
 # This script is used to create the database and the user for the wordpress
 service mysql start;
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
-mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
+echo "lol1"
+mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};" -p$MYSQL_ROOT_PASSWORD
+echo "lol2"
+mysql -e "CREATE USER IF NOT EXISTS ${MYSQL_USER}@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" -p$MYSQL_ROOT_PASSWORD
+echo "lol3"
+mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';" -p$MYSQL_ROOT_PASSWORD
+echo "lol4"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" -p$MYSQL_ROOT_PASSWORD
+echo "lol5"
+mysql -e "FLUSH PRIVILEGES;" -p$MYSQL_ROOT_PASSWORD
 mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
 exec mysqld_safe
--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
+# Faire un healthcheck pour attendre que le service mariadb soit up
 sleep 10
+rm -f /var/www/wordpress/wp-config.php
+
 if [ ! -e /var/www/wordpress/wp-config.php ]; then
+	echo "Configuring wordpress..."
+	echo "${MYSQL_DATABASE}<->${MYSQL_USER}<->${MYSQL_PASSWORD}"
 	wp config create --allow-root \
 		--dbname=${MYSQL_DATABASE} \
 		--dbuser=${MYSQL_USER} \
 		--dbpass=${MYSQL_PASSWORD} \
-		--dbhost=mariadb:3306 --path='/var/www/wordpress'
+		--dbhost=mariadb --path='/var/www/wordpress'
+# sleep 2
+# wp core install     --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root --path='/var/www/wordpress'
+# wp user create      --allow-root --role=author $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS --path='/var/www/wordpress' >> /log.txt
 fi
 
 /usr/sbin/php-fpm7.3 -F

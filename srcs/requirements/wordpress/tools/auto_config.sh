b6aeec9f1c3e77cee14fff08eaddfb6c148faac6
genouf <gabriel.enouf@yahoo.fr>
2023-02-23 12:05:40 +0100

    [SAVE 42]

--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Faire un healthcheck pour attendre que le service mariadb soit up
-sleep 10
-rm -f /var/www/wordpress/wp-config.php
+sleep 12
+# rm -f /var/www/wordpress/wp-config.php
 
 if [ ! -e /var/www/wordpress/wp-config.php ]; then
 	echo "Configuring wordpress..."
@@ -12,9 +12,9 @@ if [ ! -e /var/www/wordpress/wp-config.php ]; then
 		--dbuser=${MYSQL_USER} \
 		--dbpass=${MYSQL_PASSWORD} \
 		--dbhost=mariadb --path='/var/www/wordpress'
-# sleep 2
-# wp core install     --url=$DOMAIN_NAME --title=$SITE_TITLE --admin_user=$ADMIN_USER --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL --allow-root --path='/var/www/wordpress'
-# wp user create      --allow-root --role=author $USER1_LOGIN $USER1_MAIL --user_pass=$USER1_PASS --path='/var/www/wordpress' >> /log.txt
+sleep 4
+wp core install     --url=$WEBSITE_N --title=$SITE_T --admin_user=$ADM_U --admin_password=$ADM_P --admin_email=$ADM_E --allow-root --path='/var/www/wordpress'
+wp user create      --allow-root --role=author $FIRST_L $FIRST_M --user_pass=$FIRST_P --path='/var/www/wordpress' >> /log.txt
 fi
 
 /usr/sbin/php-fpm7.3 -F
